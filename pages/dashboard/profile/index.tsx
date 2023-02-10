f2c05524f13d2ad387129aa37a32f6dba4b4bd51
Shajibul Alam Shihab <82690571+shihab-2021@users.noreply.github.com>
2023-02-10 17:37:49 +0600

    profile update form

---
+++ pages/dashboard/profile/index.tsx
@@ -0,0 +1,12 @@
+import MainProfile from '@/Components/Dashboard/Profile/MainProfile';
+import React from 'react';
+
+const Profile = () => {
+    return (
+        <div>
+            <MainProfile></MainProfile>
+        </div>
+    );
+};
+
+export default Profile;
